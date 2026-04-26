4ef9c7d9b3f553de54268cfe4e28b17212f5d761
autofollower350 <followerauto350@gmail.com>
2026-04-26 15:05:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
-# Telegram Account Api Id And Api Hash
-API_ID = 28590286          # यहाँ अपनी API ID लिखें (नंबर में)
-API_HASH = "6a68cc6b41219dc57b7a52914032f92f"    # यहाँ अपना API HASH लिखें (स्ट्रिंग में)
+# config.py का नया कोड
 
-# Your Main Bot Token 
-BOT_TOKEN = "7936101320:AAGTHSCteVyYUzPb-snNWXDn9MxQDZUXs1M" # यहाँ अपना बोट टोकन लिखें
+API_ID = 28590286  # यहाँ कोट्स ("") न लगाएं, सिर्फ नंबर लिखें
+API_HASH = "6a68cc6b41219dc57b7a52914032f92f" # यहाँ अपना हैश कोट्स के अंदर लिखें
+BOT_TOKEN = "7936101320:AAGTHSCteVyYZPb-snWXD..." # अपना टोकन लिखें
 
-# Owner ID For Broadcasting 
-OWNER_ID = 987654321       # यहाँ अपनी Telegram ID लिखें
-
-
-# Mongodb Database Uri
-MONGO_DB_URI = "mongodb+srv://Kailash979933:Lions98@cluster0.af4v4ge.mongodb.net/?appName=Cluster0" # यहाँ अपनी MongoDB URI लिखें
-
-# Port To Run Web Application 
+# बाकी सब भी इसी तरह सीधे लिखें
+OWNER_ID = 987654321
+MONGO_DB_URI = "mongodb+srv://Kailash979933:Lions98@cluster0.af4v4ge.mongodb.net/?appName=Cluster0"
 PORT = 8080
