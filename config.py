d79d0ca624f0d4c1a0dfef2538d8f5a06b0c7a51
autofollower350 <followerauto350@gmail.com>
2026-04-29 12:07:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # Telegram Account Api Id And Api Hash
-API_ID = 28590286
-API_HASH = "6a68cc6b41219dc57b7a52914032f92f"
+API_ID = 28
+API_HASH = ""
 
 # Your Main Bot Token 
-BOT_TOKEN = "7936101320:AAEB2zvfFoNTsU82mnPKsE6a9DvH4dtshXY"
+BOT_TOKEN = "793"
 
 # Owner ID For Broadcasting 
-OWNER_ID = 6117445553
+OWNER_ID = 61
 
 # Give Your Force Subscribe Channel Id Below
 F_SUB = None
 
 # Mongodb Database Uri
-MONGO_DB_URI = "mongodb+srv://Kailash979933:Lions98@cluster0.af4v4ge.mongodb.net/?appName=Cluster0"
+MONGO_DB_URI = "mongodb+srv://K933:L@cluster0.af4v4ge.mongodb.net/?appName=Cluster0"
 
 # Port To Run Web Application 
 PORT = 8080
